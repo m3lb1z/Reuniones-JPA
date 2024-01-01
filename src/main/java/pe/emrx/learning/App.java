5d954025f6b6267ad0019c14dcf0f6fdf80c9d7e
Elvis Maldonado Rodriguez <u201112959@upc.edu.pe>
2024-01-01 11:38:44 -0500

    Prerequisitos para inciar el proyecto

---
+++ src/main/java/pe/emrx/learning/App.java
@@ -0,0 +1,13 @@
+package pe.emrx.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
